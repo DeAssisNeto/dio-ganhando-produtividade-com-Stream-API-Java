130954674f232b686c34a41af9154054eb390fe0
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-05-14 16:56:18 -0300

    feat: desafio 8 resolvido.

---
+++ ganhando-produtividade-com-Stream-API-Java/src/streamapi/SomarNumeros.java
@@ -0,0 +1,18 @@
+package streamapi;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SomarNumeros {
+    private List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+    public static void main(String[] args) {
+        SomarNumeros somarNumeros = new SomarNumeros();
+
+        int soma = somarNumeros.numeros.stream()
+                .reduce(0, Integer::sum);
+        System.out.println(soma);
+
+    }
+
+}
