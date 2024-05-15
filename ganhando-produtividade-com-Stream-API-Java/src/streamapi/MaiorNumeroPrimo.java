7b74b381f539d87b3ec2cb35e7c8cbfe87d5a9d4
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-05-15 13:27:05 -0300

    feat desafio 14 resolvido.

---
+++ ganhando-produtividade-com-Stream-API-Java/src/streamapi/MaiorNumeroPrimo.java
@@ -0,0 +1,30 @@
+package streamapi;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class MaiorNumeroPrimo {
+    private List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+    @Override
+    public String toString() {
+        return "MaiorNumeroPrimo{" +
+                "numeros=" + numeros +
+                '}';
+    }
+
+
+    public static void main(String[] args) {
+        MaiorNumeroPrimo maiorNumeroPrimo = new MaiorNumeroPrimo();
+
+        int maiorNumero = maiorNumeroPrimo.numeros.stream()
+                .filter(num -> num == 2 || num % 2 != 0 && num != 1)
+                .filter(num -> (num == 3 || num % 3 != 0) && (num == 5 || num % 5 != 0) && (num == 7 || num % 7 != 0))
+                .max(Comparator.naturalOrder())
+                .get();
+
+        System.out.println(maiorNumero);
+    }
+}
